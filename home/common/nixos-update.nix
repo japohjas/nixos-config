7c675cd46e29cb1352fc2af55e950cf9e58dabde
Jari Pohjasmaki <japohjas@gmail.com>
2024-06-14 16:06:44 +0300

    up-nix

--- home/common/nixos-update.nix
+++ home/common/nixos-update.nix
@@ -14,10 +14,10 @@
       cd ..
       echo ""
 
-      echo "nix flake update ~/nixos-config"
-      nix flake update ~/nixos-config
+      # echo "nix flake update ~/nixos-config"
+      # nix flake update ~/nixos-config
 
-      echo "nixos-rebuild switch --flake"
+      echo "nixos-rebuild switch --flake ~/nixos-config"
       sudo nixos-rebuild switch --flake ~/nixos-config
   
       echo "sudo -k"

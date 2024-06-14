4a1dbbbe343e043f716b86b8aca8ab5ff6e9bfba
Jari Pohjasmaki <japohjas@gmail.com>
2024-06-14 03:01:54 +0300

    intel

--- modules/gpu-intel.nix
+++ modules/gpu-intel.nix
@@ -3,12 +3,12 @@
 {
   boot.initrd.kernelModules = [ "i915" ];
 
+  # https://wiki.nixos.org/wiki/Accelerated_Video_Playback
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      libvdpau-va-gl
+      intel-media-driver # For Broadwell (2015) or newer processors. LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # For older processors. LIBVA_DRIVER_NAME=i965
     ];
   };
 }

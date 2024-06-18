58f5166d632e5e3469950b004d8eb13969f94b16
Jari Pohjasmaki <japohjas@gmail.com>
2024-06-18 11:41:17 +0300

    flake-update

---
+++ home/common/nixos-flake-update.nix
@@ -0,0 +1,34 @@
+{ config, ... }:
+
+{
+  home.file.".nixos-update" = {
+    text = ''
+      echo "NixOS flake + full päivitys käynnistetty..."
+      echo ""
+
+      echo "nix flake update ~/nixos-config"
+      nix flake update ~/nixos-config
+
+      echo "nixos-rebuild switch --flake ~/nixos-config"
+      sudo nixos-rebuild switch --flake ~/nixos-config
+  
+      echo "sudo -k"
+      sudo -k
+      echo "Poistettu sudo oikeudet"
+      echo ""
+
+      echo "flatpak update -y"
+      flatpak update -y
+      echo ""
+
+      echo "pipx upgrade yle-dl"
+      pipx upgrade yle-dl
+      echo ""
+
+      echo "Päivitä github" 
+      echo "Viimeistele päivitys käynnistämällä kone uudestaan."
+    '';
+    executable = true;
+    recursive = true;
+  };
+}

164389f24e73d57a856012fb818162fec78b5a06
Jari Pohjasmaki <japohjas@gmail.com>
2024-07-12 01:37:06 +0300

    calendar font size 18pt

--- home/toshiba-l300-home.nix
+++ home/toshiba-l300-home.nix
@@ -31,10 +31,10 @@
 
   # xfce calendar
   # calendar:indeterminate {color: gray; }
+  # calendar.highlight { backg round-color: gray; }
   home.file.".config/gtk-3.0/gtk.css" = {
     text = ''
-      calendar { font.size: 16pt; padding: 5px; margin: 5px; }
-      calendar.highlight {background-color: gray;}
+      calendar { font.size: 18pt; padding: 5px; margin: 5px; }
     '';
     recursive = true;
   };  

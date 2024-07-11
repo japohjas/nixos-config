295bbf794d550df5b82702a74cc0506e8ad4510d
Jari Pohjasmaki <japohjas@gmail.com>
2024-07-11 18:32:55 +0300

    new

---
+++ home/programs/pkgs-xfce.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }: 
+
+{
+  home.packages = (with pkgs; [ 
+    gnome.gnome-disk-utility
+    gnome.gnome-calculator
+
+    cinnamon.xreader # a PDF viewer
+    cinnamon.pix # a photo organization application based on gThumb
+    # cinnamon.xviewer # an image viewer based on Eye of GNOME
+    # xed-editor # a text editor
+  ]);

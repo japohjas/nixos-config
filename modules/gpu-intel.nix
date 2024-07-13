5c38435f8c074e5acea6f9905d6e2f928cd2aa76
Jari Pohjasmaki <japohjas@gmail.com>
2024-07-13 16:48:08 +0300

    added libvdpau-va-gl

--- modules/gpu-intel.nix
+++ modules/gpu-intel.nix
@@ -9,6 +9,7 @@
     extraPackages = with pkgs; [
       intel-media-driver # For Broadwell (2015) or newer processors. LIBVA_DRIVER_NAME=iHD
       intel-vaapi-driver # For older processors. LIBVA_DRIVER_NAME=i965
+      libvdpau-va-gl
     ];
   };
 }

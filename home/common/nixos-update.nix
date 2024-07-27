3379418972d9a06a7f43018ab8c0f3cf47a6de27
Jari Pohjasmaki <japohjas@gmail.com>
2024-07-27 14:29:12 +0300

    Running garbage collection after wiping history

--- home/common/nixos-update.nix
+++ home/common/nixos-update.nix
@@ -17,6 +17,14 @@
       # echo "nix flake update ~/nixos-config"
       # nix flake update ~/nixos-config
 
+      echo "Deleting all historical versions older than 7 days"
+      sudo nix profile wipe-history --older-than 7d --profile /nix/var/nix/profiles/system
+      echo ""
+
+      echo "Running garbage collection after wiping history"
+      sudo nix-collect-garbage --delete-old
+      echo ""
+
       echo "nixos-rebuild switch --flake ~/nixos-config"
       sudo nixos-rebuild switch --flake ~/nixos-config
   
